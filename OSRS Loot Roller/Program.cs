3913fde623fa66fedfcd11c24dfcca60f17df961
Dylan Weaver <dweaver@umd.edu>
2017-08-09 13:31:33 -0400

    Implented an Item object instead of using a dict

--- OSRS Loot Roller/Program.cs
+++ OSRS Loot Roller/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OSRS_Loot_Roller
 {
     class Program
     {
-        static Dictionary<string, int> lootTable = new Dictionary<string, int>(); //Stores the item and it's droprate (1/int)
+        static List<Item> lootTable = new List<Item>(); //Stores the item
         static string itemToRoll = ""; // The item the user wants to roll for
         static bool found = false; // Have we found the item yet?
         static int numberOfTries = 0; // The current number of rolls
@@ -14,28 +15,11 @@ namespace OSRS_Loot_Roller
         // Add all of the items and their drop rates to lootTable
         static void initializeLootTable()
         {
-            lootTable.Add("Ranger Boots", 293);
+            lootTable.Add(new Item("Ranger Boots", 293));
         }
 
-        // Generate a random roll until we get the item we want
-        static void processItemToRoll(string itemToProcess)
+        static void askReroll()
         {
-            Random rndNum = new Random();
-            randomNumber = rndNum.Next(1, lootTable[itemToProcess] + 1);
-            while (!found)
-            {
-                if(randomNumber != lootTable[itemToProcess])
-                {
-                    numberOfTries++;
-                    Console.WriteLine($"Rolling for {itemToProcess}, current dry streak: " + numberOfTries);
-                    randomNumber = rndNum.Next(1, lootTable[itemToProcess] + 1);
-                }
-                else
-                {
-                    Console.WriteLine($"Got {itemToProcess}! It took " + (numberOfTries + 1) + " clues!");
-                    found = true;
-                }
-            }
             Console.WriteLine("Would you like to try again? [y/n]");
             String rollAgain = Console.ReadLine();
             if (rollAgain.Equals("y", StringComparison.OrdinalIgnoreCase) || rollAgain.Equals("yes", StringComparison.OrdinalIgnoreCase))
@@ -49,6 +33,28 @@ namespace OSRS_Loot_Roller
             else return;
         }
 
+        // Generate a random roll until we get the item we want
+        static void processItemToRoll(Item itemToProcess)
+        {
+            Random rndNum = new Random();
+            randomNumber = rndNum.Next(1, itemToProcess.DropRate);
+            while (!found)
+            {
+                if(randomNumber != itemToProcess.DropRate)
+                {
+                    numberOfTries++;
+                    Console.WriteLine($"Rolling for {itemToProcess.Name}, current dry streak: " + numberOfTries);
+                    randomNumber = rndNum.Next(1, itemToProcess.DropRate + 1);
+                }
+                else
+                {
+                    Console.WriteLine($"Got {itemToProcess.Name}! It took " + (numberOfTries + 1) + " clues!");
+                    found = true;
+                }
+            }
+            askReroll();
+        }
+
         static void runRoller()
         {
             Console.WriteLine("What item would you like to roll for?");
@@ -57,18 +63,25 @@ namespace OSRS_Loot_Roller
 
             if (itemToRoll.Equals("/list"))
             {
-                foreach (KeyValuePair<string, int> item in lootTable)
+                foreach (Item item in lootTable)
                 {
-                    Console.WriteLine(item.Key);
+                    Console.WriteLine(item);
                 }
                 return;
             }
-
-            foreach (KeyValuePair<string, int> item in lootTable)
+            if (!lootTable.Any(item => item.Name.Equals(itemToRoll, StringComparison.OrdinalIgnoreCase)))
             {
-                if (item.Key.Equals(itemToRoll, StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("That item is not in the loot table, would you like to try a different item?");
+                askReroll();
+            }
+            else
+            {
+                foreach (Item item in lootTable)
                 {
-                    processItemToRoll(item.Key);
+                    if (item.Name.Equals(itemToRoll, StringComparison.OrdinalIgnoreCase))
+                    {
+                        processItemToRoll(item);
+                    }
                 }
             }
         }
